b9028f80835760b06ec876841de3f1d487216ca6
haidahan <39579398+haidahan@users.noreply.github.com>
2018-05-24 10:59:04 +0300

    Add files via upload

---
+++ task1.tf
@@ -0,0 +1,65 @@
+##############
+#Provider SET#
+##############
+
+provider "aws" {
+  access_key = "AKIAJJHQCZVMFSYL3EOA"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
+#######################
+#Education declaration#
+#######################
+
+resource "aws_iam_role" "Engineering" {
+  name = "Engineering"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {"AWS": "*"},
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+#####################
+#Finance declaration#
+#####################
+
+resource "aws_iam_role" "Finance" {
+  name = "Finance"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {"AWS": "*"},
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+###################
+#Initial atachment#
+###################
+
+resource "aws_iam_role_policy_attachment" "Finance-policy-attachment" {
+  role       = "${aws_iam_role.Finance.name}"
+  policy_arn = "arn:aws:iam::aws:policy/job-function/Billing"
+}
+resource "aws_iam_role_policy_attachment" "Engineering-policy-attachment" {
+  role       = "${aws_iam_role.Engineering.name}"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
